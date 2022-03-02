c2e378d67eeecb93a2a4e9b5f198b52adcdbb68a
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2022-03-02 10:41:37 +0300

    Tutorial 2 + 3

--- README.md
+++ README.md
@@ -6,6 +6,30 @@
     > mkdir Websites
 
 2. Then to install Wagtail
-    1. inside our project **`only`**
+    1. In our project **`only`**
         > pip install wagtail
+    2. To install it on your device **`Globally`** just type the same code in your terminal in the **`Root`**
 
+3. To start your first wagtail project
+    > wagtail start mysite
+
+    mysite here is the name of your project
+
+4. Then cd to the project
+
+5. Then enter the shell
+    > pipenv shell
+
+6. Then type the following code to extract your dependency to the requirements file for the docker
+    > pip install -r requirements.txt
+
+------------------------------------------
+
+## How To Run Wagtail
+
+1. First we have to migrate
+    > python manage.py migrate
+2. Then we should create the superuser
+    > python manage.py createsuperuser
+3. Then we can run the server
+    > python manage.py runserver
--- home/models.py
+++ home/models.py
@@ -1,14 +1,42 @@
 from dataclasses import Field
-from re import template
+from pyexpat import features
+from re import T, template
+from tabnanny import verbose
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel , PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
     templates = 'home/home_page.html' # Choose the template to conect the model to
-    banner_title = models.CharField(max_length=255 , blank=False , null=True) # add new field to the model
+    banner_titles = models.CharField(max_length=255 , blank=False , null=True) # Add new field to the model
+    banner_subtitle = RichTextField(features=["bold", "italic"]) # Add a subtitle to the banner
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    ) # Add image choose field
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    ) # Add Call To Action Field Where we can choose a page to redirect to when we click on banner
+
+    max_count = 1 # To have only one home page in the site
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title")
+        FieldPanel("banner_titles"),
+        FieldPanel("banner_subtitle"),
+        PageChooserPanel("banner_cta"),
+        ImageChooserPanel("banner_image")
     ] # to show the field in the admin panel
+
+    class Meta: # class to give information about the information
+        verbose_name = "Home Page" # To change the page Title
+        verbose_name_plural = "Home Pages"
