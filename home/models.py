0cbb661bc88f2ee10845ab67dcd6b6954beba572
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2022-03-05 15:05:59 +0300

    Tutorial 11 + 12 + 13

--- home/models.py
+++ home/models.py
@@ -1,11 +1,39 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page ,Orderable
 from wagtail.core.fields import RichTextField , StreamField
-from wagtail.admin.edit_handlers import FieldPanel , PageChooserPanel , StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel , PageChooserPanel , StreamFieldPanel , InlinePanel , MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+from modelcluster.fields import ParentalKey
 
 from streams import blocks
+
+class HomePageCarouselBanner(Orderable):
+
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    carousel_title = models.CharField(max_length=60,blank=False,null=True)
+    carousel_subtitle = models.TextField(max_length=250,blank=False,null=True)
+    carousel_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
+    panels = [
+        ImageChooserPanel("carousel_image"),
+        FieldPanel("carousel_title"),
+        FieldPanel("carousel_subtitle"),
+        PageChooserPanel("carousel_link"),
+    ]
 
 class HomePage(Page):
     templates = 'home/home_page.html' # Choose the template to conect the model to
@@ -29,6 +57,7 @@ class HomePage(Page):
         [
             ("title_and_text" , blocks.TitleAndTextBlock()), # ("The name of the stream field you can name it any thing but once you did you can't change it cause wagtail use it", Add out block from streams)
             ("full_richtext" , blocks.RichTextBlock()),
+            ("card" , blocks.CardsBlock()),
         ],
         null=True,
         blank=True
@@ -37,11 +66,25 @@ class HomePage(Page):
     max_count = 1 # To have only one home page in the site
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_titles"),
-        FieldPanel("banner_subtitle"),
-        PageChooserPanel("banner_cta"),
-        ImageChooserPanel("banner_image"),
-        StreamFieldPanel("content")
+        MultiFieldPanel(
+            [
+                FieldPanel("banner_titles"),
+                FieldPanel("banner_subtitle"),
+                PageChooserPanel("banner_cta"),
+                ImageChooserPanel("banner_image"),      
+            ],
+            heading="Banner Options"
+        ),
+        MultiFieldPanel(
+            [
+                InlinePanel("carousel_images",max_num=5 , min_num=1, label="Image"),
+                # InlinePanel("carousel_title", label="Image"),
+                # InlinePanel("carousel_subtitle", label="Image"),
+                # InlinePanel("carousel_subtitle", label="Image"),
+            ],
+            heading="Carousel Images"
+        ),
+        StreamFieldPanel("content"),
     ] # to show the field in the admin panel
 
     class Meta: # class to give information about the information
